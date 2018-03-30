585cbe45db89302cd0d7a4ac7a36a0c0548b95ba
Unknown <kaptainwutax@gmail.com>
2018-03-30 16:35:11 -0400

    Added keybindings

--- src/main/java/com/srkw/tweakoni/utils/handlers/RegistryHandler.java
+++ src/main/java/com/srkw/tweakoni/utils/handlers/RegistryHandler.java
@@ -1,13 +1,17 @@
 package com.srkw.tweakoni.utils.handlers;
 
+import org.lwjgl.input.Keyboard;
+
 import com.srkw.tweakoni.init.BlockInit;
 import com.srkw.tweakoni.init.ItemInit;
 import com.srkw.tweakoni.utils.interfaces.IHasModel;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -20,12 +24,25 @@ public class RegistryHandler {
 	}
 
 	public static void initRegistries() {
-
+		
+		registerKeyBindings();
 		
 	}
 
 	public static void postInitRegistries() {
 
+		
+	}
+	
+	public static void registerKeyBindings () {
+		
+		KeyBinding[] keyBindings = new KeyBinding[2]; 
+		 
+		keyBindings[0] = new KeyBinding("key.toggle.shift", Keyboard.KEY_L, "key.tweaktoni.category");
+		  
+		for (int i = 0; i < keyBindings.length; ++i) {
+		    ClientRegistry.registerKeyBinding(keyBindings[i]);
+		}
 		
 	}
 	
