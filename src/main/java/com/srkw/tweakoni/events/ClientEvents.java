44b08a13ae19bc98e68ff71e1a2aa34d53d33915
StrikerRocker <thariksherief1@gmail.com>
2018-04-05 03:28:44 +0530

    one step close towards rendering the durability of Elytra

--- src/main/java/com/srkw/tweakoni/events/ClientEvents.java
+++ src/main/java/com/srkw/tweakoni/events/ClientEvents.java
@@ -4,17 +4,26 @@ import com.srkw.tweakoni.network.PacketHandler;
 import com.srkw.tweakoni.network.PacketSendLoc;
 import com.srkw.tweakoni.proxy.ClientProxy;
 import com.srkw.tweakoni.utils.RayTrace;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.RenderItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemElytra;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@SuppressWarnings("MethodCallSideOnly")
+@SuppressWarnings("ALL")
 @Mod.EventBusSubscriber
 public class ClientEvents {
+
+    private static Minecraft mc = Minecraft.getMinecraft();
+
     @SubscribeEvent
     public static void onRightClick(PlayerInteractEvent.RightClickBlock event) {
         if (event.getWorld().isRemote) {
@@ -23,6 +32,23 @@ public class ClientEvents {
                 RayTraceResult result = RayTrace.rayTrace(event.getWorld(), event.getEntityPlayer(), false);
                 if (result.sideHit == EnumFacing.UP) {
                     PacketHandler.INSTANCE.sendToServer(new PacketSendLoc());
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onOverlayRender(RenderGameOverlayEvent.Post event) {
+        if (event.getType().equals(RenderGameOverlayEvent.ElementType.TEXT)) {
+            ScaledResolution resolution = event.getResolution();
+            int x = resolution.getScaledWidth() / 100;
+            int y = resolution.getScaledHeight() / 100;
+            for (ItemStack stack : mc.player.inventory.armorInventory) {
+                if (stack.getItem() instanceof ItemElytra) {
+                    String text = "Elytra : " + stack.getItemDamage() + " / " + stack.getMaxDamage();
+                    mc.fontRenderer.drawStringWithShadow(text, x / 95, y / 98, 0xffffff);
+                    RenderItem itemRenderer = Minecraft.getMinecraft().getRenderItem();
+                    //itemRenderer.renderItemAndEffectIntoGUI(stack, x / 94, y / 98);
                 }
             }
         }
