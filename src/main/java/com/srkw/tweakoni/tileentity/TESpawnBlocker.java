f6b1ac9a1c98a8c20164d0e33d39f3b9aab12e39
Unknown <kaptainwutax@gmail.com>
2018-04-08 15:20:10 -0400

    Got the blocker working in a 3x3 area. Issue : doesnt work a\on newly spawned mobs

--- src/main/java/com/srkw/tweakoni/tileentity/TESpawnBlocker.java
+++ src/main/java/com/srkw/tweakoni/tileentity/TESpawnBlocker.java
@@ -4,36 +4,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.monster.*;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.chunk.Chunk;
+import scala.Console;
 
 public class TESpawnBlocker extends TileEntity implements ITickable {
 	
 	@Override
 	public void update() {
 	
-		int x1 = world.getChunkFromBlockCoords(pos).getPos().getXStart();
+		//Sets the first corner of the box
+		int x1 = world.getChunkFromBlockCoords(pos).getPos().getXStart() - 16;
 		int y1 = 0;
-		int z1 = world.getChunkFromBlockCoords(pos).getPos().getZStart();
+		int z1 = world.getChunkFromBlockCoords(pos).getPos().getZStart() - 16;
 		
-		int x2 = world.getChunkFromBlockCoords(pos).getPos().getXEnd();
+		//Sets the second corner of the box
+		int x2 = world.getChunkFromBlockCoords(pos).getPos().getXEnd() + 16;
 		int y2 = 256;
-		int z2 = world.getChunkFromBlockCoords(pos).getPos().getXEnd();
+		int z2 = world.getChunkFromBlockCoords(pos).getPos().getZEnd() + 16;
 		
-		List<EntityZombie> list = new ArrayList<>();
-		
+		//Stores the box in an AxisAlignedBB
 		AxisAlignedBB AABB = new AxisAlignedBB(new BlockPos(x1, y1, z1), new BlockPos(x2, y2, z2));
-
-		world.getChunkFromBlockCoords(pos).getEntitiesOfTypeWithinAABB(EntityZombie.class, AABB, list, null);
 		
-		for(EntityZombie zombie : list) zombie.setDead();
+		//Reference for all entities in the AABB
+		List<Entity> list = new ArrayList<>();
+		list = world.getEntitiesWithinAABBExcludingEntity(null, AABB);
 		
-		//world.getChunkFromBlockCoords(pos).removeEntity(new EntityZombie(world));
-		System.out.println("Update method being called");
+		//Loops through the entity list and kills all the unwanted ones
+		for(Entity entity : list) {
+			
+			if(entity instanceof EntityZombie||entity instanceof EntitySpider||entity instanceof EntityCreeper||entity instanceof EntitySkeleton||entity instanceof EntitySlime||entity instanceof EntityEnderman)
+				entity.setDead();
+			
+			Console.println("got it");
+			
+		}
 		
 	}
 
