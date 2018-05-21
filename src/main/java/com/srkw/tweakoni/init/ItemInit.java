0a670a5cac8b0ebdf80cfa872fef5eb162080cb4
Unknown <kaptainwutax@gmail.com>
2018-05-21 17:01:25 -0400

    ItemFrame works

--- src/main/java/com/srkw/tweakoni/init/ItemInit.java
+++ src/main/java/com/srkw/tweakoni/init/ItemInit.java
@@ -1,6 +1,5 @@
 package com.srkw.tweakoni.init;
 
-import com.srkw.tweakoni.item.ItemItemFrame;
 import com.srkw.tweakoni.item.ItemRotator;
 import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
@@ -9,19 +8,17 @@ public class ItemInit
 {
 
     public static final ItemRotator ROTATOR = new ItemRotator("rotator");
-    public static final ItemItemFrame ITEM_FRAME = new ItemItemFrame("item_frame");
 
     public static void register(IForgeRegistry<Item> registry)
     {
         registry.registerAll(
-                ROTATOR, ITEM_FRAME
+                ROTATOR
         );
     }
 
     public static void registerModels()
     {
         ROTATOR.registerItemModel();
-        ITEM_FRAME.registerItemModel();
         
     }
 }
