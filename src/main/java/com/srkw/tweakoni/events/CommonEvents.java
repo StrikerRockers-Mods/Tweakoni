87b95864e33a10322fbe54a48f228c3f15d81f7a
Unknown <kaptainwutax@gmail.com>
2018-05-22 18:15:23 -0400

    Villagers are now leashable (Except the stupid Nitwit, no one loves him)

--- src/main/java/com/srkw/tweakoni/events/CommonEvents.java
+++ src/main/java/com/srkw/tweakoni/events/CommonEvents.java
@@ -1,16 +1,25 @@
 package com.srkw.tweakoni.events;
 
 import com.srkw.tweakoni.block.JukeBoxProvider;
+import com.srkw.tweakoni.init.ItemInit;
+
 import net.minecraft.block.BlockJukebox.TileEntityJukebox;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraft.block.BlockMagma;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -21,6 +30,36 @@ import static com.srkw.tweakoni.Tweakoni.MOD_ID;
 public class CommonEvents
 {
 
+	 @SubscribeEvent
+	 public static void onRightClickEntity(PlayerInteractEvent.EntityInteract event) {
+		 
+		 if(event.getTarget() instanceof EntityVillager && event.getEntity() instanceof EntityPlayer && !event.getWorld().isRemote) {
+		 
+			 EntityVillager l = (EntityVillager) event.getTarget();
+			 EntityPlayer p = (EntityPlayer) event.getEntity();
+			 EnumHand h = p.getActiveHand();
+			 
+		 	if (l.getLeashed() && l.getLeashHolder() == p)
+		 	{
+			 	l.clearLeashed(true, !p.capabilities.isCreativeMode);
+			 	event.setCanceled(true);
+			 	return;
+		 	}
+		 	else
+		 	{
+			 	ItemStack itemstack = p.getHeldItem(h);
+
+			 	if (itemstack.getItem() == ItemInit.LEAD)
+			 	{
+				 	l.setLeashHolder(p, true);
+				 	itemstack.shrink(1);
+				 	event.setCanceled(true);
+				 	return;
+			 	}			 	
+		 	}
+	 	}
+	 }
+	 
     @SubscribeEvent
     public static void onLivingUpdate(LivingEvent.LivingUpdateEvent event)
     {
